7d6ff9adb08681e8e404d9ff6ada84c44200823e
Nick1st <Nick1st.dev@gmail.com>
2019-10-02 17:41:38 +0200

    Some Test

--- Sieve.py
+++ Sieve.py
@@ -1,14 +1,41 @@
 def main():
 
     # Get maximum numbers of prims.
-print("Test")
 
-    while true:
+    while True:
         try:
-            maximum = int(raw_input('Maximum: '))
+            maximum = int(input('Maximum: '))
             #maximum = int(10)
             print(maximum)
             break
 
         except ValueError:
             print("Not allowed! Use an Integer as Input!")
+
+    numbers = [True]*(maximum+1)
+    #Remove 0 and 1
+    numbers[0] = False
+    numbers[1] = False
+    a = []
+
+    i = 2
+    while i*i <= maximum:
+        if numbers[i] == True:
+            #Mark number as prim
+            #Kill all multiplie of prim.
+            for k in range(i*i,maximum+1,i):
+                numbers[k] = False
+
+        i = i+1
+
+        # Ausgabe aller gefundenen Zahlen
+    for i, v in enumerate(numbers):
+        if v == True:
+            a.append(i)
+            print (i, ',')
+
+    print(a)
+    return 0
+
+if __name__ == '__main__':
+    main()
