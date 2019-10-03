4919415d4fa8f5a2b9483c07dc6f4173ad922369
Nick1st <Nick1st.dev@gmail.com>
2019-10-03 16:59:09 +0200

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from prime_number_gen import Prime_gen
 
-prime_number = Prime_gen()
+prime_number1 = Prime_gen()
 
-print(prime_number)
+print(prime_number1)
---
+++ test_gcd.py
@@ -0,0 +1,4 @@
+
+def correct_gcd(to_test_number,test_number):
+    loop="true"
+    while loop==true
