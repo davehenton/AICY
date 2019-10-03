959309e81d90c11bf324d7c982e27062a9ba3533
Nick1st <Nick1st.dev@gmail.com>
2019-10-03 12:17:27 +0200

    Some descriptions

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from v_big_numbers import prim_gen_min as pgmi, prim_gen_max as pgma
 from test_random_number_odd import gen_n
+from random_int_gen import GenRandom
 
-random_number_n = gen_n()
+random_number_n = gen_n(pgmi,pgma) #Generates an odd, random, 150 digits, positive number.
+random_number_a = GenRandom(2,random_number_n-1) #Generates a random positive number, that is smaller than random_number_n.
 
 print(random_number_n)
+print('')
+print(random_number_a)
--- test_random_number_odd.py
+++ test_random_number_odd.py
@@ -1,11 +1,9 @@
 from random_int_gen import GenRandom
-from v_big_numbers import prim_gen_min as pgmi, prim_gen_max as pgma
 
-
-def gen_n(): #Generates n new until it is odd. Doesn't require any input.
+def gen_n(minimal_number,maximal_number): #Generates n new until it is odd. Doesn't require any input.
     loop = "true"
     while loop=="true":
-        random_number_n = GenRandom(pgmi,pgma) #Gives Funktion GenRandom pgmi and pgma as input.
+        random_number_n = GenRandom(minimal_number,maximal_number) #Gives Funktion GenRandom minimal and maximal number as input.
         #print(random_number_n)
         tmp_modulo_result = random_number_n%2 #Divides Output of GenRandom with 2.
         if tmp_modulo_result==1: #Breaks the Loop if random_number_n is odd.
