e22d3176933a506928e8dd18d0f0444005643411
Nick1st <Nick1st.dev@gmail.com>
2019-10-12 21:39:48 +0200

    Python...

---
+++ console/commands_py/help.py
@@ -0,0 +1,5 @@
+def f_help():
+    print()
+
+if __name__ == '__main__':
+    f_help()
--- console/commands_py/startup.py
+++ console/commands_py/startup.py
@@ -1,5 +1,5 @@
 print()
 print('Hello to the:             Text-Coding-Libary              ')
 print()
-print("If you need any Help, type: 'help'")
+print("If you need any help, type: 'help'!")
 print()
