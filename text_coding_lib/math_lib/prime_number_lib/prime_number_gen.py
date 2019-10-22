874d8cb5da7740226ac3a7a33664b811ab9768af
Nick1st <Nick1st.dev@gmail.com>
2019-10-22 21:08:28 +0200

    ...

---
+++ text_coding_lib/math_lib/prime_number_lib/prime_number_gen.py
@@ -0,0 +1,11 @@
+def prime_number_generator(check_level=1):
+    if check_level=1:
+        #Code here
+
+    elif check_level=2:
+        #Code here
+
+    elif check_level=3:
+        #Code here
+
+    else return 'Error'
