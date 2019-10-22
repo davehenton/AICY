c86b46484b60260bf949a89980106025da4420b8
Nick1st <Nick1st.dev@gmail.com>
2019-10-22 16:56:46 +0200

    Stocked code

--- text_coding_lib/math_lib/basic_math_lib/divisor_module.py
+++ text_coding_lib/math_lib/basic_math_lib/divisor_module.py
@@ -1,3 +1,9 @@
+def isOdd(number_to_be_tested): #Returns True if a number is odd and False if not. Will fail if input isn't an INTEGER.
+    if number_to_be_tested%2==1:
+        return True
+    elif number_to_be_tested%2==0:
+        return False
+
 def isDivisor(dividend,divisor): #Returns True if the dividend is divisible by the divisor, else it returns False. Will fail as soon as one of the inputs isn't an INTEGER.
     if dividend%divisor==0:
         return True
@@ -33,3 +39,18 @@ def thirteenIsDivisor(dividend): #Returns True if the dividend is divisible by 1
         return True
     else:
         return False
+
+def greatest_common_divisor(number_one,number_two): #Gets the gcd of two numbers.
+    loop="true"
+    while loop=="true": #Calculates the gcd.
+        tmp_divisor = number_two//number_one #Dived the greater trough the smaler number without rest.
+        tmp_rest = number_two%number_one #Dived the greater trough the smaller number and takes the rest.
+        if (tmp_rest==0): #If gcd was found, break the loop.
+            gcd=number_one
+            loop="false"
+            break
+        else: #If gcd wasn't found, do the steps above again.
+            number_two = number_one
+            number_one = tmp_rest
+
+    return(gcd) #Return gcd.
