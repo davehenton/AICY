94984ad81562ac27179fb6270c08267f02a5a3bf
Nick1st <Nick1st.dev@gmail.com>
2019-10-22 15:13:19 +0200

    Finished Help in console

--- console/commands_py/help.py
+++ console/commands_py/help.py
@@ -2,7 +2,34 @@ import argparse
 tmp = ''
 
 parser = argparse.ArgumentParser(prog="help",description='Prints the help.',add_help=True)
-parser.add_argument('-c', '--command', default=argparse.SUPPRESS, choices=['gcd'], help='shows the help for a specific command')
-parser.set_defaults(func=help)
+parser.add_argument('-c', '--command', choices=['gcd'], help='shows the help for a specific command')
+#parser.set_defaults(func=help)
+args = parser.parse_args()
 
-parser.parse_args()
+if args.command is None:
+    print()
+    print('     Thank you for using the     TEXT-CODING-LIBARY      :')
+    print(
+    '''
+        If you want more detailed information
+        about a specified command, type:
+            'help -c [specified command]
+
+        Possible [specified command]s are:
+            - 'gcd'
+
+        For the usage of this command, type:
+            'help -h' or 'help --help'
+    ''')
+
+if args.command == 'gcd':
+    print(
+    '''
+        The 'gcd' command founds you the
+            greatest common divisor of two
+            numbers.
+
+        USAGE:
+            gcd [int1] [int2]
+    '''
+    )
