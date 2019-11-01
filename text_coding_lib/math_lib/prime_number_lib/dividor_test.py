3b64f1990c35208861b059420d7cd28e5c341af6
Nick1st <Nick1st.dev@gmail.com>
2019-11-01 11:16:20 +0100

    Finished dividor_test up_to variable.

--- text_coding_lib/math_lib/prime_number_lib/dividor_test.py
+++ text_coding_lib/math_lib/prime_number_lib/dividor_test.py
@@ -1,30 +1,35 @@
 def dividor_test(up_to): #Uses the dividor test. Requires a maximum number.
+    up_to_n = up_to+1
     prime = [2] #Inititalization of the prime number space.
     lenght = len(prime) #Gets object count in prime.
-    print(lenght)
+    #print(lenght)
     tested_primes = 0 #Saves the number of already tested primes.
     current = 3 #Current number
     loop = True #Starts the loop
-    loop2 = True
 
     while loop == True: #This is the real funktion.
-        if tested_primes < lenght:
-            print(1)
-            if current%prime[tested_primes] != 0:
-                print(1.1)
-                tested_primes = tested_primes+1
-            elif current%prime[tested_primes] == 0:
-                print(current%prime[tested_primes])
-                print(1.2)
+        if up_to_n != current:
+            if tested_primes < lenght:
+                #print(1)
+                if current%prime[tested_primes] != 0:
+                    #print(1.1)
+                    tested_primes = tested_primes+1
+                elif current%prime[tested_primes] == 0:
+                    #print(current%prime[tested_primes])
+                    #print(1.2)
+                    tested_primes = 0
+                    current = current+1
+            elif tested_primes >= lenght:
+                #print(2)
+                prime.append(current)
                 tested_primes = 0
                 current = current+1
-        elif tested_primes >= lenght:
-            print(2)
-            prime.append(current)
-            tested_primes = 0
-            current = current+1
-            lenght = len(prime)
+                lenght = len(prime)
+                #print(prime)
+        elif up_to_n == current:
+            loop = False
             print(prime)
+            print("Finished")
 
 if __name__ == '__main__':
-    dividor_test(10)
+    dividor_test(100)
