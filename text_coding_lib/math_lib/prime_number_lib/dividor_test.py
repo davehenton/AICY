fa4ebb79ac3894d4fcf9b157d6b32c4be245e3e1
Nick1st <Nick1st.dev@gmail.com>
2019-11-01 10:32:56 +0100

    Finished dividor_test module.

--- text_coding_lib/math_lib/prime_number_lib/dividor_test.py
+++ text_coding_lib/math_lib/prime_number_lib/dividor_test.py
@@ -1,5 +1,4 @@
-def dividor_test():#up_to=10): #Uses the dividor test. Requires a maximum number.
-    up_to=10
+def dividor_test(up_to): #Uses the dividor test. Requires a maximum number.
     prime = [2] #Inititalization of the prime number space.
     lenght = len(prime) #Gets object count in prime.
     print(lenght)
@@ -9,15 +8,23 @@ def dividor_test():#up_to=10): #Uses the dividor test. Requires a maximum number
     loop2 = True
 
     while loop == True: #This is the real funktion.
-        if tested_primes >= lenght:
-            if current%prime[tested_primes] == current:
+        if tested_primes < lenght:
+            print(1)
+            if current%prime[tested_primes] != 0:
+                print(1.1)
                 tested_primes = tested_primes+1
-            elif current%prime[tested_primes] != current:
+            elif current%prime[tested_primes] == 0:
+                print(current%prime[tested_primes])
+                print(1.2)
                 tested_primes = 0
                 current = current+1
-        elif tested_primes <= lenght:
-            prime = prime.append(current)
-            loop = False
+        elif tested_primes >= lenght:
+            print(2)
+            prime.append(current)
+            tested_primes = 0
+            current = current+1
+            lenght = len(prime)
+            print(prime)
 
 if __name__ == '__main__':
-    dividor_test()
+    dividor_test(10)
