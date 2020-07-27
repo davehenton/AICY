af6d6438c00c26b45118598219ff377ca9a9bd39
Nick1st <Nick1st.dev@gmail.com>
2020-07-27 09:26:42 +0200

    .

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,4 +1,15 @@
-from src.hello import hello
+#from src.hello import hello
 
-def test_hello():
-    assert "Hello" == hello()
+#def test_hello():
+#    assert "Hello" == hello()
+
+import time
+
+def test_this():
+    try:
+        time.sleep(10)
+    except Exception:
+        pass
+
+def test_that():
+    pass
