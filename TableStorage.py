fc26c4b53703274e0dcc4ac2224fe925d028f073
Nick1st <Nick1st.dev@gmail.com>
2020-07-27 12:26:09 +0200

    Cleanups

--- TableStorage.py
+++ TableStorage.py
@@ -2,7 +2,7 @@
 CONTACTS__1_0_pre = ["contacts", [["id", "int"], ["publicName", "text"], ["publicSurname", "text"],
                                 ["privateName", "text"], ["privateSurName", "text"], ["privateStatus", "text"],
                                 ["picture", "blob"], ["friendship", "int"], ["favorite", "int"]]]
-def contacts__1_0_pre(c):
+def contacts__1_0_pre(c, mode: int):
     #c.execute('CREATE TABLE contacts (id int, publicName text, publicSurname text, privateName text, privateSurName text, privateStatus text, picture blob, friendship int, favorite int)')
     #c.execute('DROP TABLE contacts')
     print("contacts__1_0_pre(c)")
